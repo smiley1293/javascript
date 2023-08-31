513392f6d9aa81c95e99df2865ab0983f582706d
VantCii <viet12092003@gmail.com>
2023-09-01 00:52:58 +0700

    create lesson 10

---
+++ lesson_11/main.js
@@ -0,0 +1,12 @@
+/**
+ * * Mảng trong JavaScript - Array
+ * 
+ * 1. Tạo mảng
+ *       - Cách tạo
+ *       - Sử dụng cách nào? Tại sao?
+ *       - Kiểm tra data type
+ * 2. Truy xuất mảng
+ *       - Độ dài mảng
+ *       - Lấy phần tử theo index
+ */
+
