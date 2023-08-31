9fa40d284290960d0a25323f8cb74970f517e526
VantCii <viet12092003@gmail.com>
2023-09-01 01:21:18 +0700

    new array

--- lesson_11/main.js
+++ lesson_11/main.js
@@ -10,3 +10,35 @@
  *       - Lấy phần tử theo index
  */
 
+var languages = [
+   'JavaScript',
+   'PHP',
+   'Ruby',
+]
+
+var languages2 = new Array(
+   'JavaScript',
+   'PHP',
+   'Ruby',
+);
+
+console.log(languages[0])
+console.log(languages.length)
+console.log(languages2)
+console.log(typeof languages)
+
+console.log(Array.isArray(languages))
+
+/**
+ * *Làm việc với Array
+ * 
+ * 1. To string
+ * 2. Join
+ * 3. Pop
+ * 4. Push
+ * 5. Shift
+ * 6. Unshift
+ * 7. Splicing
+ * 8. Concat
+ * 9. Slicing
+ */
