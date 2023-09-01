b7d6d6cbd35b42741c39c66ffa5711ede329730a
VantCii <viet12092003@gmail.com>
2023-09-01 16:38:38 +0700

    object

--- lesson_12/main.js
+++ lesson_12/main.js
@@ -68,5 +68,75 @@ User.prototype.className = 'F8';
 User.prototype.getClassName = function (){
    return this.className;
 }
-console.log(author.getClassName())
-console.log(user)
+console.log(author.getClassName());
+console.log(user);
+
+//* Đối tượng Date
+
+var date = new Date();
+
+console.log(date)
+var currentDay = date.toISOString().split('T')[0]
+console.log(currentDay)
+
+var year = date.getFullYear();
+var month = date.getMonth() + 1;
+var day = date.getDate()
+
+console.log(year)
+console.log(month)
+console.log(day)
+
+// Key word: Javascript date object mozilla
+
+
+/**
+ * *Math object
+ * 
+ * - Math.PI
+ * - Math.round()
+ * - Math.abs()
+ * - Math.ceil()
+ * - Math.floor()
+ * - Math.random()
+ * - Math.min()
+ * - Math.max()
+ */
+
+console.log(Math.PI)
+console.log(Math.round(1.2)) // 1 : làm tròn theo quy luật
+console.log(Math.abs(-4)) //lấy giá trị tuyệt đối
+console.log(Math.ceil(9.00001)) // 10 | hàm ceil luôn làm tròn lên, chỉ cần có thập phân là đơm
+console.log(Math.floor(9.6)) // thằng này luôn làm tròn xuống dưới
+console.log(Math.random())// ứng đụng để tạo ra 1 số ngẫu nhiên
+console.log(Math.floor(Math.random()*10))
+var random = Math.floor(Math.random()*100);
+// chơi theo tỉ lệ
+if(random < 5){ //tỉ lệ thành công là 5%
+   console.log('Cường hóa thành công')
+}else{
+   console.log('Bạn đen lắm')
+}
+var bonus = [
+   '10 coin',
+   '20 coin',
+   '30 coin',
+   '40 coin',
+   '50 coin',
+]
+console.log(bonus[random])
+
+console.log(Math.min(1,2,3,4,5,))
+// Max thì ngược lại
+function getRandomItem(array){
+   var random = Math.floor(Math.random()*array.length)
+   return array[random]
+}
+var sale = [
+   '10%',
+   '20%',
+   '30%',
+   '40%',
+   '50%',
+]
+console.log(getRandomItem(sale));
