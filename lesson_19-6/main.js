bd6dec239deaad2c46a1ff4a11fbb3632710b8d9
VantCii <viet12092003@gmail.com>
2023-09-07 03:58:03 +0700

    node properties

---
+++ lesson_19-6/main.js
@@ -0,0 +1,11 @@
+var boxElement = document.querySelector('.box');
+
+console.log(boxElement)
+
+// autocapitalize: tự cho chữ viết hoa
+// childElementCOunt: đếm thằng con của element
+// classList: quản lí class, kiểm tra xem có class hay ko
+// daraggable: dành cho nghiệp vụ kéo thả
+// firstChild: text node or element node
+// nextElementSibling: chỉ trả về thằng element tiếp theo
+// nextSibling: trả về node tiếp theo gồm text node hoặc element node
