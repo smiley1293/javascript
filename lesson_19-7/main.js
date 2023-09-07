460e78df811809b8efc8c72d75f89c5c0e16da55
VantCii <viet12092003@gmail.com>
2023-09-08 00:40:46 +0700

    DOM CSS

--- lesson_19-7/main.js
+++ lesson_19-7/main.js
@@ -1,8 +1,19 @@
 //* DOM Style
 
-var boxElement = document.querySelector('.box');
-// console.log(boxElement.style)
+var boxElement = document.querySelector('.box .hello');
+boxElement.style.color = 'red'
 
+console.log(boxElement.style)
 
+// boxElement.style;
+// boxElement.style.height='100px';
+// boxElement.style.backgroundColor='red';
+// boxElement.style.width = '200px'
 
+// Object.assign(boxElement.style,{
+//    width:'200px',
+//    height:'100px',
+//    backgroundColor:'red',
+//    display:''
+// })
 
