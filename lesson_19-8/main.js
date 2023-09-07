de80c6ee6a57bae64092e804533de2186e70a11b
VantCii <viet12092003@gmail.com>
2023-09-08 01:10:56 +0700

    classList property

---
+++ lesson_19-8/main.js
@@ -0,0 +1,18 @@
+//* ClassList Property
+
+// add: thêm class vào trong element
+// contains: kiểm tra xem có chứa class hay ko
+// remove: xóa bỏ 1 class khỏi element
+// toggle: nếu đang có class ở element, thì gọi toggle nó xóa, ko có thì nó sẽ thêm
+
+var boxElement = document.querySelector('.box')
+
+console.log(boxElement.classList.add('red'))
+console.log(boxElement.classList.contains('red'))
+console.log(boxElement.classList.remove('red'))
+// console.log(boxElement.classList.toggle('red'))
+// console.log(boxElement.classList.toggle('red'))
+
+setInterval(()=>{
+   boxElement.classList.toggle('red')
+},1000)
