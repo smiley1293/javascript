980243f34b1cb7cf427034421ea2fb17cb604e4c
VantCii <viet12092003@gmail.com>
2023-09-13 17:44:36 +0700

    Tagged template literals

---
+++ lesson_21-3/main.js
@@ -0,0 +1,15 @@
+// *Tagged template literals
+
+function highlight([first,...strings], ...values) {
+   return values.reduce(
+      (acc, curr) => [...acc,`<span>${curr}</span>`, strings.shift()],[first]
+   ).join('');
+}
+var brand = 'F8';
+var course = 'ReactJS'
+
+const html = highlight`
+Học lập trình ${course} tại ${brand} 
+`;
+
+console.log(html);
